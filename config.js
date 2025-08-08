b6828ca864a8f715256d7d54b63692b17d0493c3
Mannelo133 <maximealassa@gmail.com>
2025-08-08 07:28:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "YOUR SESSION_ID",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~Ds5hQLzY#1Qkp8nJHGcercifEqPthYm179laCIwEDLP4mLrJrcLs",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
